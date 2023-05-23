f2d0048e047df0964428a7a08c54af3b5977db32
Mateis <mateis.pro@outlook.fr>
2023-05-23 20:32:58 +0200

    Login Functionality

--- app/api/auth/[...nextAuth]/route.ts
+++ app/api/auth/[...nextAuth]/route.ts
@@ -53,7 +53,7 @@ export const authOptions: AuthOptions = {
 	session: {
 		strategy: "jwt"
 	},
-	secret: process.env.NEXTAUTH_URL
+	secret: process.env.NEXTAUTH_SECRET
 }
 
 const handler = NextAuth(authOptions);
